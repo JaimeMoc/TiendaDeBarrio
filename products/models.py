6f61722a62b4fda517139ce8afe93ed1e249e0a8
Jaime Alberto Suarez Moctezuma <Suarezjaime2712@gmail.com>
2025-01-07 13:32:30 -0600

    Modificamos la estructura de "Products" en donde creamos una referencia para "Brand" y la ajustamos a la BD.

--- products/models.py
+++ products/models.py
@@ -1,13 +1,19 @@
 from django.utils import timezone   
 from django.db import models
 
-#Creación de la clase producto en donde definiremos sus atributos.
+#Creación de la clase "Producto" en donde definiremos sus atributos.
 class Product(models.Model):
     name = models.CharField(max_length=50)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     sku = models.CharField(max_length=10)
     category = models.CharField(max_length=50)
-    brand = models.CharField(max_length=50)
+    
+    #Creamos una clave foránea para referencia a Brand
+    brand = models.ForeignKey(
+        'products.Brand', 
+        on_delete=models.CASCADE,
+        related_name='products'
+    )
     
     image = models.ImageField(
         blank=True, null=True,
@@ -21,3 +27,20 @@ class Product(models.Model):
     #Función que nos permitira que se muestren los nombres de los productos junto a la marca. 
     def __str__(self):
         return f'{self.name} | {self.brand}'
+  
+#Creación de la clase "Brand" en donde definiremos sus atributos.   
+class Brand(models.Model):
+    text = models.CharField(max_length=30)
+    description = models.TextField(null=True, blank=True)
+    
+    logo = models.ImageField(
+        blank=True, null=True,
+        upload_to='media/products'                      
+        )
+    
+    created_date = models.DateField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+    
+    def __str__(self):
+        return self.name
+    
