5bbfdf9f9f135827f192f13afbe38ade86d1ce3e
Jaime Alberto Suarez Moctezuma <Suarezjaime2712@gmail.com>
2025-01-06 18:37:32 -0600

    Cambios a la bd

--- my_store/settings.py
+++ my_store/settings.py
@@ -76,9 +76,11 @@ WSGI_APPLICATION = 'my_store.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "OPTIONS": {
+            "read_default_file": "C:/Users/jaime/OneDrive/Escritorio/Django-Codigo-facilito/my_store/my.cnf",
+        },
     }
 }
 
