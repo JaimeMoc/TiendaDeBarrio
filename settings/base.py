918b1fb3eb878fe09e3364d15a591bacb4d6ed17
Jaime Alberto Suarez Moctezuma <Suarezjaime2712@gmail.com>
2025-01-06 22:50:37 -0600

    cambios en el archivo Settings. Se crearon tres archivos para administrarlo.

--- settings/base.py
+++ settings/base.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-fdaltzam#63z@n4i-wkzy1g%gbqc=o=q8j$tbc@5fs_5r(p^ho
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -71,20 +69,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'my_store.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "OPTIONS": {
-            "read_default_file": "C:/Users/jaime/OneDrive/Escritorio/Django-Codigo-facilito/my_store/my.cnf",
-        },
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
@@ -103,18 +87,16 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Mexico_City'
 
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
---
+++ settings/local.py
@@ -0,0 +1,11 @@
+from .base import *
+
+# Configuración de la base de datos MySQL.
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "OPTIONS": {
+            "read_default_file": "C:\Users\jaime\OneDrive\Escritorio\Django-Codigo-facilito\settings\my.cnf",
+        },
+    }
+}
