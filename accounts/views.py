85e2871ffbd374108e1f20df7441ee952382cde0
Jaime Alberto Suarez Moctezuma <Suarezjaime2712@gmail.com>
2025-01-14 18:19:02 -0600

    Agregamos la sección de login dentro de nuestra página web.

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import AuthenticationForm
 
 def login_view(request):
     
     if request.method == "POST":
         pass
-        
-    return render(request, 'accounts/login.html')
+    
+    form = AuthenticationForm()
+    return render(request, 'accounts/login.html', {'form': form})
