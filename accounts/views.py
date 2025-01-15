6b17e2644755b61db640b49617240e0696a7290e
Jaime Alberto Suarez Moctezuma <Suarezjaime2712@gmail.com>
2025-01-14 19:17:50 -0600

    Agregamos la funcionalidad "Logout" dentro de nuestro index.

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, logout
 
 def login_view(request):
-    
     if request.method == "POST":
-        pass
-    
-    form = AuthenticationForm()
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user= form.get_user()
+            login(request, user)
+            return redirect('index')
+    else: 
+        form = AuthenticationForm()
     return render(request, 'accounts/login.html', {'form': form})
+
+def logout_view(request):
+    if request.method == "POST":
+        logout(request)
+        return redirect('index')
