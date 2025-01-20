875d6974753221dc2c7bd21b08de78bbb9ed83dd
Jaime Alberto Suarez Moctezuma <Suarezjaime2712@gmail.com>
2025-01-20 15:38:11 -0600

    Creamos la vista de Signup, junto a su archivo html, y lo mencionamos en las urls. Este nos permitira manejar el acceso

--- accounts/views.py
+++ accounts/views.py
@@ -9,7 +9,6 @@ def signup_view(request):
         
         if form.is_valid:
             user = form.save()
-        
             login(request,user)
             return redirect('index')
         
