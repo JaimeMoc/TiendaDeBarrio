b7458f0840b27e94c90931a189f84e87ffe8abea
Jaime Alberto Suarez Moctezuma <Suarezjaime2712@gmail.com>
2025-01-20 11:59:18 -0600

    Creamos el forms que nos ayudara a tener nuestros formularios.

---
+++ products/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+
+from .models import Comment
+
+class CommentsForm(forms.ModelForm):
+    
+    class Meta:
+        model = Comment
+        
+        fields = ('text', )
