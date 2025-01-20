6ffd43fde42cfdc85db062cfeef5b7f7a8d414ff
Jaime Alberto Suarez Moctezuma <Suarezjaime2712@gmail.com>
2025-01-20 14:45:30 -0600

    Cambios en la urls y las views para poder ver el formulario y poder cargarlo.

--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from .models import Product, Comment
 from .forms import CommentsForm
 
@@ -20,3 +20,21 @@ def get_product(request, id):
                   {'product': product,
                    'comments': comments,
                    'form': form})
+
+@login_required
+def add_new_comment(request, id):
+    if request.method == 'POST':
+        
+        form = CommentsForm(request.POST)
+        
+        if form.is_valid:
+            user = request.user
+            product = Product.object.get(id=id)
+            
+            new_comment = form.save(commit=False)
+            new_comment.author = user 
+            new_comment.product = product
+            
+            new_comment.save()
+            
+    return redirect('get_product', id)
