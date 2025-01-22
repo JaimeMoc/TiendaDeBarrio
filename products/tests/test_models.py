e546ca463a7242fe7212a5bb01634f9f3c63c054
Jaime Alberto Suarez Moctezuma <Suarezjaime2712@gmail.com>
2025-01-21 21:35:27 -0600

    Test para Products.

--- products/tests/test_models.py
+++ products/tests/test_models.py
@@ -4,28 +4,28 @@ from products.models import Product, Brand
 
 class ProductModelTestCase(TestCase):
     def setUp(self):
-        #Crea una instancia de Brand para asociarla al producto.
+        # Crea una instancia de Brand para asociarla al producto.
         self.brand = Brand.objects.create(name='Mi Marca')
-        #Crea una instancia de Product para probar
+        # Crea una instancia de Product para probar
         self.product = Product.objects.create(
-            name = "Mi Producto",
-            price = 10.50,
-            sku = 'SKU123',
-            category = 'Electronicos',
-            brand = self.brand,
-            discount = 5,
-            created_date = timezone.now(),
-            published_date = timezone.now()
-            )
+            name="Mi Producto",
+            price=10.50,
+            sku='SKU123',
+            category='Electronicos',
+            brand=self.brand,
+            discount=5,
+            created_date=timezone.now(),
+            published_date=timezone.now()
+        )
         
     def test_product_str_method(self):
-        #Verficia si el método __str__ devuelve el formato esperado.
-        expected_str = f'{self.product.name}  | {self.product.brand}'
+        # Verifica si el método __str__ devuelve el formato esperado.
+        expected_str = f'{self.product.name} | {self.product.brand}'
         self.assertEqual(str(self.product), expected_str)
         
     def test_product_attributes(self):
-        #Verifica si los atributos del producto son correctos. 
-        self.assertEqual(self.product.name, 'Mi producto')
+        # Verifica si los atributos del producto son correctos.
+        self.assertEqual(self.product.name, 'Mi Producto')
         self.assertEqual(self.product.price, 10.50)
         self.assertEqual(self.product.sku, 'SKU123')
         self.assertEqual(self.product.category, 'Electronicos')
@@ -33,4 +33,3 @@ class ProductModelTestCase(TestCase):
         self.assertEqual(self.product.discount, 5)
         self.assertIsNotNone(self.product.created_date)
         self.assertIsNotNone(self.product.published_date)
-        
