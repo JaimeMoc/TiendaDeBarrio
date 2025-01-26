50569f6c1f1529567a3520556e3f08205c513085
Jaime Alberto Suarez Moctezuma <Suarezjaime2712@gmail.com>
2025-01-26 16:01:55 -0600

    Creacción de un carrito de compras

--- my_store/urls.py
+++ my_store/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', include('products.urls')),
     path('accounts/', include('accounts.urls')),
     path('admin/', admin.site.urls),
+    path('cart/', include('cart.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
