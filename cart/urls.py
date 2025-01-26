fdd24a8c34c664c814ccc448f541412b0cb13aa6
Jaime Alberto Suarez Moctezuma <Suarezjaime2712@gmail.com>
2025-01-26 17:58:51 -0600

    Implementamos en el HTML de Show_product la funcionalidad de añadir al carrito.

--- cart/urls.py
+++ cart/urls.py
@@ -5,7 +5,7 @@ app_name = 'cart'
 
 urlpatterns = [
     path('', views.cart_detail, name='cart_detail'),
-    path('add/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
+    path('add_to_cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
     path('remove/<int:product_id>/', views.remove_from_cart, name='remove_from_cart'),
     path('update/<int:product_id>/<int:quantity>/', views.update_cart, name='update_cart'),
 ]
